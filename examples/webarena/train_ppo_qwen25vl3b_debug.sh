3613d8f661b54577c0c602e49bd9f47dccb9a0d2
YuRuiii <1754376900@qq.com>
2026-03-13 19:34:05 -0500

    edit port

--- examples/webarena/train_ppo_qwen25vl3b_debug.sh
+++ examples/webarena/train_ppo_qwen25vl3b_debug.sh
@@ -2,6 +2,14 @@
 
 set -x
 
+export SHOPPING="http://localhost:7770"
+export SHOPPING_ADMIN="http://localhost:7780/admin"
+export GITLAB="http://localhost:8023"
+export REDDIT="http://localhost:9999"
+export WIKIPEDIA="http://localhost:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing"
+export MAP="http://localhost:3000"
+export HOMEPAGE="http://localhost:4399"
+
 PROJECT_NAME="verl_vagen"
 EXPERIMENT_NAME="webarena_ppo_qwen25vl3b"
 
