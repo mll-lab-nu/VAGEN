f843346ecdbcf243f4d2d617da1309f1f80a332e
YuRuiii <1754376900@qq.com>
2026-05-08 03:35:12 -0500

    fix

--- examples/train/webarena/train_webarena_grpo_qwen25_3b.sh
+++ examples/train/webarena/train_webarena_grpo_qwen25_3b.sh
@@ -44,6 +44,8 @@ export MKL_NUM_THREADS=4
 export TOKENIZERS_PARALLELISM=false
 export HF_HOME=${HF_HOME:-/workspace/hf_cache}
 
+
+which python
 PYTHONUNBUFFERED=1 python -m vagen.main_ppo \
     --config-path=${BASEDIR}/vagen/configs \
     --config-name='vagen_multiturn' \
